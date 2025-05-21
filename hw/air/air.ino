d13f48d67c7fccf68bd0c9e6f491dc7f23bb6a46
khwon14 <khywo0814@gmail.com>
2025-05-21 22:32:07 +0900

    air sensor_v_1

--- hw/air/air.ino
+++ hw/air/air.ino
@@ -1,9 +1,28 @@
+int GasPin = A0;
+const int level_warning = 500;
+
 void setup() {
   // put your setup code here, to run once:
 //air
+  pinMode(GasPin, INPUT);
+  Serial.begin(9600); //시리얼 초기화
+  int level_warning = 500;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  float value = get_ppm();
 
+  led_blinking();
+  if (value > level_warning)
+  {
+    Serial.println("ALERT");
+  }
+  
+  delay(1000);
+}
+
+float get_ppm() {
+  int raw = analogRead(GasPin);
+  
 }
