807e2c7a694163e08297b7c0eb1854da67643d73
khwon14 <khywo0814@gmail.com>
2025-05-23 20:18:29 +0900

    air_sensor_v_2

--- hw/air/air.ino
+++ hw/air/air.ino
@@ -24,5 +24,9 @@ void loop() {
 
 float get_ppm() {
   int raw = analogRead(GasPin);
-  
+   float voltage = raw * (5.0 / 1023.0);
+
+  // 예제 공식: 측정 보정 필요
+  float ppm = (voltage - 0.1) * 1000.0;
+  return ppm;
 }
