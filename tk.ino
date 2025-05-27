8b5d492d6ffb82bcc23f2280ff6b7fe98a2b5b65
ktk0208 <kafakim1@gmail.com>
2025-05-27 12:16:53 +0900

    Add files via upload

---
+++ tk.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup() {
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+}
+
+void loop() {
+  while (ss.available() > 0) {
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()) {
+      Serial.print("Latitude: ");
+      Serial.println(gps.location.lat(), 6);
+      Serial.print("Longitude: ");
+      Serial.println(gps.location.lng(), 6);
+    }
+  }
+}
