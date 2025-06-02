dc6697373b85162330338aad2d70f301e687d1e6
minjun018 <eptwl@daum.net>
2025-06-02 23:02:06 +0900

    final

--- miji/Heartbeat/Heartbeat.ino
+++ miji/Heartbeat/Heartbeat.ino
@@ -1,24 +1,35 @@
+
+
 int pulsePin = A0;       // Pulse Sensor가 연결된 핀
 int signal;              // 측정된 신호 값
 int threshold = 550;     // 박동 감지 임계값 (상황에 따라 조정 필요)
 
+
 unsigned long lastBeatTime = 0;
+
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+
 int beatCount = 0;
 int bpm = 0;
 
 void setup() {
   Serial.begin(9600);
+  ss.begin(GPSBaud);
   pinMode(pulsePin, INPUT);
 }
 
 void loop() {
   signal = analogRead(pulsePin);
+  
+
+  Serial.println();
 
   // 박동 감지: threshold 초과 + 최소 간격(300ms) 유지
   if (signal > threshold && millis() - lastBeatTime > 300) {
     lastBeatTime = millis();
     beatCount++;
-    Serial.println("♥ Beat detected");
+    Serial.println("70");
   }
 
   // 10초간 측정 후 BPM 계산
